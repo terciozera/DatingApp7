62fd3e2152a51fca94c45ed6c7800658bd6232ae
terciozera <tercio.link.united@gmail.com>
2023-10-27 15:38:03 +0100

    End of section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,38 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { User } from './_models/user';
-import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements OnInit{
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
-  
+  constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
-    this.getUsers();
-    this.setCurrentUser();
-  }
-  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next:  response => this.users = response,
+      next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
-    }) 
-  }
- 
-  
-  setCurrentUser() {
-    const userString = localStorage.getItem('user')
-    if (!userString) return;
-    const user: User = JSON.parse(userString);
-    this.accountService.setCurrentUser(user);
+    })
   }
 
+  
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,27 +5,19 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    BsDropdownModule.forRoot()
+    BrowserAnimationsModule
   ],
-  
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+                       
