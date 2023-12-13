8d3afc8287d6f72812977693a4b07ae324ea9a14
terciozera <tercio.link.united@gmail.com>
2023-12-13 11:39:14 +0000

    End of Section 8

--- API/Data/Mingrations/20231129152446_ExtentedUserEntity.Designer.cs
+++ API/Data/Mingrations/20231129152446_ExtentedUserEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Mingrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231129152446_ExtentedUserEntity")]
+    partial class ExtentedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
@@ -44,7 +47,7 @@ namespace API.Data.Mingrations
                     b.Property<string>("Introduction")
                         .HasColumnType("TEXT");
 
-                    b.Property<string>("KnownAs")
+                    b.Property<string>("KnowAs")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("LastActive")
